74ddd45a10514cb1a9a020241a09a0f9eea5bf0c
Jun Xiang Tee <juntee@google.com>
2018-10-23 11:26:06 -0700

    change bgd test to test against metadata.generation

--- examples/bluegreen/test.sh
+++ examples/bluegreen/test.sh
@@ -24,12 +24,14 @@ until kubectl get $bgd; do sleep 1; done
 echo "Create an object..."
 kubectl apply -f my-bluegreen.yaml
 
+# TODO(juntee): change observedGeneration steps to compare against generation number when k8s 1.10 and earlier retire.
+
 echo "Wait for nginx-blue RS to be active..."
 until [[ "$(kubectl get rs nginx-blue -o 'jsonpath={.status.readyReplicas}')" -eq 3 ]]; do sleep 1; done
 until [[ "$(kubectl get rs nginx-green -o 'jsonpath={.status.replicas}')" -eq 0 ]]; do sleep 1; done
 until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.activeColor}')" -eq "blue" ]]; do sleep 1; done
 until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.active.availableReplicas}')" -eq 3 ]]; do sleep 1; done
-until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.observedGeneration}')" -eq 1 ]]; do sleep 1; done
+until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.observedGeneration}')" -eq "$(kubectl get $bgd nginx -o 'jsonpath={.metadata.generation}')" ]]; do sleep 1; done
 
 
 echo "Trigger a rollout..."
@@ -40,7 +42,7 @@ until [[ "$(kubectl get rs nginx-green -o 'jsonpath={.status.readyReplicas}')" -
 until [[ "$(kubectl get rs nginx-blue -o 'jsonpath={.status.replicas}')" -eq 0 ]]; do sleep 1; done
 until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.activeColor}')" -eq "green" ]]; do sleep 1; done
 until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.active.availableReplicas}')" -eq 3 ]]; do sleep 1; done
-until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.observedGeneration}')" -eq 2 ]]; do sleep 1; done
+until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.observedGeneration}')" -eq "$(kubectl get $bgd nginx -o 'jsonpath={.metadata.generation}')" ]]; do sleep 1; done
 
 echo "Trigger another rollout..."
 kubectl patch $bgd nginx --type=merge -p '{"spec":{"template":{"metadata":{"labels":{"new2":"label2"}}}}}'
@@ -50,4 +52,4 @@ until [[ "$(kubectl get rs nginx-blue -o 'jsonpath={.status.readyReplicas}')" -e
 until [[ "$(kubectl get rs nginx-green -o 'jsonpath={.status.replicas}')" -eq 0 ]]; do sleep 1; done
 until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.activeColor}')" -eq "blue" ]]; do sleep 1; done
 until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.active.availableReplicas}')" -eq 3 ]]; do sleep 1; done
-until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.observedGeneration}')" -eq 3 ]]; do sleep 1; done
+until [[ "$(kubectl get $bgd nginx -o 'jsonpath={.status.observedGeneration}')" -eq "$(kubectl get $bgd nginx -o 'jsonpath={.metadata.generation}')" ]]; do sleep 1; done
